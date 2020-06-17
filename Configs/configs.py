c088209b3c2a6c633a85b6f053fcf2bb8bac3f18
Zeeshan Ali <zapt1860@gmail.com>
2020-06-17 21:50:41 +0500

    Update configs.py

--- Configs/configs.py
+++ Configs/configs.py
@@ -1,18 +1,24 @@
 class Configs:
-    # Name of the dataset
-    self.DATASETNAME = "Kather_texture_2016_image_tiles_5000"
-    
-    # Google drive link to the dataset
-    self.DATALINK = "/content/drive/My Drive/" + self.DATASETNAME
+    def __init__(self):
+        # Name of the dataset
+        self.DATASETNAME = "Kather_texture_2016_image_tiles_5000"
+        
+        # Google drive link to the dataset
+        self.DATALINK = "/content/drive/My Drive/" + self.DATASETNAME
 
-    # Batch size for training and testing of a model
-    self.BATCHSIZE = 32
-    
-    # Number of epochs for training of a model
-    self.EPOCHS = 10
-    
-    # Number of classes in the dataset
-    self.NUMCLASSES = 8
-    
-    # Class names in the dataset
-    self.CLASSES = []
+        # Batch size for training and testing of a model
+        self.BATCHSIZE = 32
+        
+        # Number of epochs for training of a model
+        self.EPOCHS = 1
+        
+        # Number of classes in the dataset
+        self.NUMCLASSES = 8
+        
+        # Class names in the dataset
+        self.CLASSES = ['ADIPOSE', 'COMPLEX', 'DEBRIS', 'EMPTY', 'LYMPHO', 'MUCOSA', 'STROMA', 'TUMOR']
+
+        # Model training setting
+        self.LOSS = "sparse_categorical_crossentropy"
+        self.OPTIMIZER = "adam"
+        self.METRICS = ["accuracy"]

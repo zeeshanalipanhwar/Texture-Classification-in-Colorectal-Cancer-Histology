70476c67ccfbff52493fa9c108b4f6e5781f689c
Zeeshan Ali <zapt1860@gmail.com>
2020-06-17 09:58:21 +0500

    Create configs.py

---
+++ Configs/configs.py
@@ -0,0 +1,18 @@
+class Configs:
+    # Name of the dataset
+    self.DATASETNAME = "Kather_texture_2016_image_tiles_5000"
+    
+    # Google drive link to the dataset
+    self.DATALINK = "/content/drive/My Drive/" + self.DATASETNAME
+
+    # Batch size for training and testing of a model
+    self.BATCHSIZE = 32
+    
+    # Number of epochs for training of a model
+    self.EPOCHS = 10
+    
+    # Number of classes in the dataset
+    self.NUMCLASSES = 8
+    
+    # Class names in the dataset
+    self.CLASSES = []

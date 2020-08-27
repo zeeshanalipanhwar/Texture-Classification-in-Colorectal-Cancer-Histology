daaafc14f798b5a4111835988bd3f8a61e34da8b
Zeeshan Ali <zapt1860@gmail.com>
2020-08-27 16:30:27 +0500

    Update custom_matrics.py

--- Utils/custom_matrics.py
+++ Utils/custom_matrics.py
@@ -2,6 +2,7 @@ from keras import backend as K
 
 def f1_score(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     recall = true_positives / (possible_positives + K.epsilon())
 
